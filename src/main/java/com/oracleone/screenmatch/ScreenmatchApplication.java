7ada8b3757b7921ef48bedabefa268b1d8129ef9
Sojo506 <fsojodev@gmail.com>
2024-10-20 12:45:53 -0600

    added @Autowired notation to use queries

--- src/main/java/com/oracleone/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/oracleone/screenmatch/ScreenmatchApplication.java
@@ -4,8 +4,10 @@ import com.oracleone.screenmatch.main.Main;
 import com.oracleone.screenmatch.main.StreamExample;
 import com.oracleone.screenmatch.model.SeasonData;
 import com.oracleone.screenmatch.model.SeriesData;
+import com.oracleone.screenmatch.repository.ISeriesRepository;
 import com.oracleone.screenmatch.service.ConvertData;
 import com.oracleone.screenmatch.service.FetchApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +18,16 @@ import java.util.List;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
+    @Autowired
+    private ISeriesRepository repository;
+
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Main main = new Main();
+        Main main = new Main(repository);
         main.start();
 
         /*StreamExample streamExample = new StreamExample();
