8242db1626ea0c03591fd7ef2b71a0779df75aef
Sojo506 <fsojodev@gmail.com>
2024-10-22 10:12:21 -0600

    added derived queries

--- src/main/java/com/oracleone/screenmatch/repository/ISeriesRepository.java
+++ src/main/java/com/oracleone/screenmatch/repository/ISeriesRepository.java
@@ -1,7 +1,16 @@
 package com.oracleone.screenmatch.repository;
 
+import com.oracleone.screenmatch.model.Category;
 import com.oracleone.screenmatch.model.Series;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ISeriesRepository extends JpaRepository<Series, Long> {
+    Optional<Series> findByTitleContainsIgnoreCase(String seriesName);
+
+    List<Series> findTop5ByOrderByRatingDesc();
+
+    List<Series> findByGenre(Category category);
 }
