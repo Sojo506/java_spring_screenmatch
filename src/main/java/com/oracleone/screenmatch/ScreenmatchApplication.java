102a38ec0f9efb0ec21d40d9d4ff8460c8038e89
Sojo506 <fsojodev@gmail.com>
2024-10-23 10:45:04 -0600

    tested search episodes by title and search top 5 episodes

--- src/main/java/com/oracleone/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/oracleone/screenmatch/ScreenmatchApplication.java
@@ -1,19 +1,11 @@
 package com.oracleone.screenmatch;
 
 import com.oracleone.screenmatch.main.Main;
-import com.oracleone.screenmatch.main.StreamExample;
-import com.oracleone.screenmatch.model.SeasonData;
-import com.oracleone.screenmatch.model.SeriesData;
 import com.oracleone.screenmatch.repository.ISeriesRepository;
-import com.oracleone.screenmatch.service.ConvertData;
-import com.oracleone.screenmatch.service.FetchApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
