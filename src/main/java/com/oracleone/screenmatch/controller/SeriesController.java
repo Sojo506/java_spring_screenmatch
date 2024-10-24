76136c5516bb57e1e8a43f9f56a8fb018eae6020
Sojo506 <fsojodev@gmail.com>
2024-10-24 11:11:21 -0600

    modified getAllSeries to get Series Data

--- src/main/java/com/oracleone/screenmatch/controller/SeriesController.java
+++ src/main/java/com/oracleone/screenmatch/controller/SeriesController.java
@@ -1,13 +1,32 @@
 package com.oracleone.screenmatch.controller;
 
+import com.oracleone.screenmatch.dto.SeriesDTO;
+import com.oracleone.screenmatch.model.Series;
+import com.oracleone.screenmatch.repository.ISeriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class SeriesController {
+    @Autowired
+    private ISeriesRepository repository;
 
     @GetMapping("/series")
-    public String showMessage() {
-        return "Hello motha foca";
+    public List<SeriesDTO> getAllSeries() {
+        return (List<SeriesDTO>) repository.findAll().stream()
+                .map(s -> new SeriesDTO(
+                        s.getTitle(),
+                        s.getGenre(),
+                        s.getActors(),
+                        s.getPlot(),
+                        s.getPoster(),
+                        s.getSeasons(),
+                        s.getRating()
+                ))
+                .collect(Collectors.toList());
     }
 }
