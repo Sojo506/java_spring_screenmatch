c4a2112a895d32c551daa6fa4064ec0deff6cbe6
Sojo506 <fsojodev@gmail.com>
2024-10-24 10:17:49 -0600

    refactored to work with spring web

--- src/main/java/com/oracleone/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/oracleone/screenmatch/ScreenmatchApplication.java
@@ -8,21 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-
-    @Autowired
-    private ISeriesRepository repository;
+public class ScreenmatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Main main = new Main(repository);
-        main.start();
 
-        /*StreamExample streamExample = new StreamExample();
-        streamExample.showExample();*/
-    }
 }
