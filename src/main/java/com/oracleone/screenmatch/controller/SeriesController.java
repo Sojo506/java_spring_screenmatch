39ed5cc4e01a2024c484bf89d396b6343daa921b
Sojo506 <fsojodev@gmail.com>
2024-10-25 12:50:27 -0600

    added main route and route /releases

--- src/main/java/com/oracleone/screenmatch/controller/SeriesController.java
+++ src/main/java/com/oracleone/screenmatch/controller/SeriesController.java
@@ -4,22 +4,30 @@ import com.oracleone.screenmatch.dto.SeriesDTO;
 import com.oracleone.screenmatch.service.SeriesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SeriesController {
     @Autowired
     private SeriesService service;
 
-    @GetMapping("/series")
+
+    @GetMapping("")
     public List<SeriesDTO> getAllSeries() {
         return service.getAllSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SeriesDTO> getTop5() {
         return service.getTop5();
+    }
+
+    @GetMapping("/releases")
+    public List<SeriesDTO> getRecentReleases() {
+        return service.getRecentReleases();
     }
 }
