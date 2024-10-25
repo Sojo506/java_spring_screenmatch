8740bb59e42b57ed27283fa749618f1468caebb4
Sojo506 <fsojodev@gmail.com>
2024-10-25 10:31:02 -0600

    refactored

--- src/main/java/com/oracleone/screenmatch/controller/SeriesController.java
+++ src/main/java/com/oracleone/screenmatch/controller/SeriesController.java
@@ -1,32 +1,20 @@
 package com.oracleone.screenmatch.controller;
 
 import com.oracleone.screenmatch.dto.SeriesDTO;
-import com.oracleone.screenmatch.model.Series;
-import com.oracleone.screenmatch.repository.ISeriesRepository;
+import com.oracleone.screenmatch.service.SeriesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SeriesController {
     @Autowired
-    private ISeriesRepository repository;
+    private SeriesService service;
 
     @GetMapping("/series")
     public List<SeriesDTO> getAllSeries() {
-        return (List<SeriesDTO>) repository.findAll().stream()
-                .map(s -> new SeriesDTO(
-                        s.getTitle(),
-                        s.getGenre(),
-                        s.getActors(),
-                        s.getPlot(),
-                        s.getPoster(),
-                        s.getSeasons(),
-                        s.getRating()
-                ))
-                .collect(Collectors.toList());
+        return service.getAllSeries();
     }
 }
