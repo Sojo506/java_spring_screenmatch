be3de31673e8eeb169b695dd55f669bb2f156a61
Sojo506 <fsojodev@gmail.com>
2024-10-25 09:22:39 -0600

    modified path

--- src/main/java/com/oracleone/screenmatch/config/CorsConfiguration.java
+++ src/main/java/com/oracleone/screenmatch/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.aluracursos.screenmatch.config;
+package com.oracleone.screenmatch.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
