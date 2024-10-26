b38a93fa23aa312c0ee20500278b639803426fd6
Sojo506 <fsojodev@gmail.com>
2024-10-26 09:48:08 -0600

    added getSeriesById

--- src/main/java/com/oracleone/screenmatch/service/SeriesService.java
+++ src/main/java/com/oracleone/screenmatch/service/SeriesService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,6 +30,7 @@ public class SeriesService {
     public List<SeriesDTO> convertData(List<Series> series) {
         return series.stream()
                 .map(s -> new SeriesDTO(
+                        s.getId(),
                         s.getTitle(),
                         s.getGenre(),
                         s.getActors(),
@@ -38,5 +40,24 @@ public class SeriesService {
                         s.getRating()
                 ))
                 .collect(Collectors.toList());
+    }
+
+    public SeriesDTO getSeriesById(Long id) {
+        Optional<Series> series = repository.findById(id);
+
+        if (series.isPresent()) {
+            Series s = series.get();
+            return new SeriesDTO(
+                    s.getId(),
+                    s.getTitle(),
+                    s.getGenre(),
+                    s.getActors(),
+                    s.getPlot(),
+                    s.getPoster(),
+                    s.getSeasons(),
+                    s.getRating());
+        }
+
+        return null;
     }
 }
